b63b9160d5681f07efc0e88982c72c20fb47fb18
huda29 <53063272+huda29@users.noreply.github.com>
2019-08-25 15:20:45 +0300

    day3

---
+++ day3.py
@@ -0,0 +1,28 @@
+x = 3 # x is of type int
+y = "huda" # x is now of type str
+print (x) 
+print (y) 
+f = 4
+f = "huda Almutairi"
+print(f)
+# "" same as ''
+x = "huda"
+x = 'huda'
+print(x)
+print(x)
+x , y , z = "C++" , "java" , "my Sql"
+print (x)
+print (y)
+print (z)
+x = y = z ="python"
+print (x)
+print (y)
+print (z)
+x = " easy "
+print ("programing" +  x)
+x = 50
+y = 50
+print(x + y)
+#x = 100
+#y = "Days of code"
+#print(x + y) 
