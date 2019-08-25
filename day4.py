ba5dec837d2142f42ebe8b1c49d9d5b90e47ad35
huda29 <53063272+huda29@users.noreply.github.com>
2019-08-25 15:21:18 +0300

    day4

---
+++ day4.py
@@ -0,0 +1,47 @@
+x = 88   # int
+y = 9.2  # float
+z = 1j   # complex
+print(type(x))
+print(type(y))
+print(type(z))
+x = 7
+y = 785768865558
+z = 2465768
+print(type(x))
+print(type(y))
+print(type(z))
+x = 5.7
+y = 6.9
+z = -155.9
+print(type(x))
+print(type(y))
+print(type(z))
+x = 30e9
+y = 22e8
+z = -170.55e7
+print(type(x))
+print(type(y))
+print(type(z))
+x = 5+7j
+y = 8j
+z = -3j
+print(type(x))
+print(type(y))
+print(type(z))
+x = 1 #int
+y = 2.8 #float
+z = 1j #complex
+#convert from int to float:
+a = float(x)
+#convert from float to int:
+b = int(y)
+#convert from int to complex:
+c = complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+import random
+print(random.randrange(1,10))
