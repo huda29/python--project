a7be8d2217b0b9fad307ddce596ddc58c5839cf2
huda29 <53063272+huda29@users.noreply.github.com>
2019-08-28 16:43:27 +0300

    day8

---
+++ day8.py
@@ -0,0 +1,12 @@
+a = " huda, Almutairi! "
+print (a.strip()) # returns "huda, Almutairi!"
+a = "huda, Almutairi!"
+print(len(a)) # returns numbers of code 
+a = "Huda, Almutairi!"
+print(a.lower()) # returns huda almutairi
+a = " Huda, Almutairi! "
+print(a.upper())  # RETURNS HUDA ALMUTAIRI
+a = " Huda, Almutairi! "
+print(a.replace("H" , "A")) # return auda
+a = " Huda, Almutairi! "
+print(a.split(",")) # return {'Huda', 'Almutairi'}
