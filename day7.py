8edc19e5f3c4f711033f40a019c626e40a5c6488
huda29 <53063272+huda29@users.noreply.github.com>
2019-08-28 14:10:41 +0300

    day7

---
+++ day7.py
@@ -0,0 +1,14 @@
+#week2
+#day7
+print("hello")
+print('hello')
+
+a=''' Python It is a high-level programming language, 
+easy to learn, linguistic programming, 
+open source, and scalable'''
+print(a)
+
+a = "Hello,world!"
+print (a[1])
+b = "Hello ,world!"
+print (b[2:5])
