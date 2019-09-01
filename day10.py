1e055aeb336b854cb1a19902bea355b0522ec7f0
huda29 <53063272+huda29@users.noreply.github.com>
2019-09-01 19:20:43 +0300

    day10

---
+++ day10.py
@@ -0,0 +1,28 @@
+#Arithmetic operators
+x=8
+y=4
+print(x + y)
+print(x - y)
+print(x ** y)
+print(x * y /2)
+print(x // y)
+print(x % y)
+
+#Assignment operators
+H = 120
+H /= 40
+m = 4
+m += 8
+print(m)
+print(H)
+
+#comparsion operators
+a= 5
+b= 1
+c= 1.0
+d= 3
+print( a != d)
+print( b == c)
+print( c > d )
+
+
