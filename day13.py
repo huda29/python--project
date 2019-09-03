30c284dd5118bff7e93841bb305a21c72b6d0c3f
huda29 <53063272+huda29@users.noreply.github.com>
2019-09-03 20:31:28 +0300

    day13

---
+++ day13.py
@@ -0,0 +1,28 @@
+#day13
+#create list
+H =[]
+print(H)
+#numbers
+numbers =[1,2,3,4,5]
+print(numbers)
+#texts
+programing =["java","php","mysql","python"]
+print(programing)
+#access item
+programing =["java","php","mysql","python"]
+print(programing[2])
+#texts with numbers
+programing =["java","php","mysql","python",1.9,2.6,3.2,4.0]
+print(programing)
+ #Loop Through a List ......Print all items in the list, one by one. 
+programing =["java","php","mysql","python"]
+for x in programing:
+    print(x)
+#Change Item Value 
+programing =["java","php","mysql","python"]
+programing[1] = "c++"
+print(programing)
+ #The del keyword removes the specified index
+programing =["java","php","mysql","python"]
+del programing[0] 
+print(programing)
