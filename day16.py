7c4407070b42eb7c841cbdd251e37a4a0a1f0566
huda29 <53063272+huda29@users.noreply.github.com>
2019-09-09 23:50:12 +0300

    day16

---
+++ day16.py
@@ -0,0 +1,20 @@
+#day 16
+#Python Tuples 
+thistuple =("ID","Phone","email","age")
+print(thistuple)
+
+thistuple =()
+print(thistuple)
+
+thistuple =(2.2, 3, 4.9, 3.3, 9)
+print(thistuple)
+
+thistuple =(2.2, 3, "huda", 9)  #The data inside a tuple can be of one or more data types.
+print(thistuple)
+ 
+thistuple =["ID","Phone","email","age"]    #Return the item in position 1.
+print(thistuple[1])
+#Change Tuple Values 
+thistuple =["ID","Phone","email","age"] 
+for x in thistuple:
+    print(x)
