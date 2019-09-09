29070ba9c57c24d4a5ca2b3ee5c4c8efec1c0cce
huda29 <53063272+huda29@users.noreply.github.com>
2019-09-10 02:02:05 +0300

    day18,19

---
+++ day18,19.py
@@ -0,0 +1,24 @@
+# challeng week
+#1
+thedayes =["monday","sunday","friday","thursday"]     
+thedayes.append("saturday")
+print(thedayes)
+
+# 2
+thedayes =["monday","sunday","friday","thursday"]     
+thedayes.pop(3)
+print(thedayes)
+
+# 3
+thedayes =["monday","sunday","friday","thursday"] 
+thedayes.remove("monday")
+print(thedayes)
+# 4
+thedayes =["monday","sunday","friday","thursday"] 
+thedayes.insert(1,"wednesday")
+print(thedayes)
+#second
+tuple = ("java","python","swift")
+if "python" in tuple:
+    print("yes,'python' is in the tuple")
+
