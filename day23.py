69fd4614e50ac86f5bace4163bd7cd760c55f4f4
huda29 <53063272+huda29@users.noreply.github.com>
2019-09-17 09:12:29 +0300

    day23

---
+++ day23.py
@@ -0,0 +1,44 @@
+#python dictionaries
+thisdict = {
+
+"brand":"food",
+
+"model":"mustange",
+
+"year" : 1999,
+
+}
+
+if "model" in thisdict:
+
+ print("yes model in there")
+
+ print(len(thisdict))
+
+ thisdict["color"] = "red"
+
+ print(thisdict)
+
+ thisdict.pop("model")
+
+ print(thisdict)
+
+ c = {
+
+     "hi":"welcome",
+
+     "huda":"thanks"
+
+ }
+
+ del c ['hi']
+
+ print(c)
+
+ c.clear()
+
+ print(c)
+
+
+
+
