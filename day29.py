ca3b602c733e147d9b12a567723f8f7f63a0a65e
huda29 <53063272+huda29@users.noreply.github.com>
2019-09-28 14:56:32 +0300

    day29

---
+++ day29.py
@@ -0,0 +1,27 @@
+#Python Loops 2
+#  For Loop 
+programs ={"c++", "java","python"}
+for x in programs:
+    print (x)
+
+#Looping Through a String 
+for x in "java":
+    print(x)
+#The break Statement 
+programs ={"c++", "java","python"}
+for x in programs:
+    print (x)
+    if x =="java":
+        break
+#The continue Statement 
+programs ={"c++", "java","python"}
+for x in programs:
+    print (x)
+    if x =="java":
+        continue
+    print(x)
+    
+
+
+ 
+ 
