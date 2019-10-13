8c53363273cb5a0ddffbe0188b9e6ca467b03785
huda29 <53063272+huda29@users.noreply.github.com>
2019-10-13 22:17:29 +0300

    day35

---
+++ day35.py
@@ -0,0 +1,12 @@
+ #Python Lambda 
+X = lambda a : a + 10
+print (X(5))
+
+#Lambda functions can take any number of arguments. 
+x = lambda a, b : a * b
+print(x(5, 6))
+
+#A lambda function that sums argument
+x = lambda a, b, c, : a + b + c 
+print(x(5,6,2))
+
