61eb9ef89e223a4ee5b78d2fb24484a94b7aeba6
huda29 <53063272+huda29@users.noreply.github.com>
2019-10-13 22:18:50 +0300

    day37

---
+++ day37.py
@@ -0,0 +1,17 @@
+#Python Arrays 
+#Access the Elements of an Array
+
+programminglanguages = ["c++", "python", "java"]
+x = programminglanguages[0]
+print(x)
+
+
+#Modify the value of the first array item
+programminglanguages = ["c++", "python", "java"]
+programminglanguages[0] = "php"
+print(programminglanguages)
+
+#The Length of an Array
+programminglanguages = ["c++", "python", "java"]
+x = len(programminglanguages)
+print(x)
