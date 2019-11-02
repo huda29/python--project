b511d3d60732acdd6577e664a907e23a27c1fd2d
huda29 <53063272+huda29@users.noreply.github.com>
2019-11-02 16:43:06 +0300

    day 64

---
+++ day64.py
@@ -0,0 +1,15 @@
+#week 10
+#Python Try-Except
+
+try:
+    print(x)
+except:
+    print('Error')
+
+#Finally
+try:
+    print(x)
+except:
+    print("somthing went worng")
+finally:
+    print("the 'ty except' is finished")  
