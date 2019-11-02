3f85c2becdaec4a0106784a8ef1f71d8bd9ef926
huda29 <53063272+huda29@users.noreply.github.com>
2019-11-02 16:46:04 +0300

    challenge of week 10

---
+++ day67,68.py
@@ -0,0 +1,13 @@
+# week 10
+# challenge of week 10
+
+name = input ('please enter the first and the end letter of your name :')
+print(f'your name is begin with {name[0]}, and end with {name[1]}')
+
+##################
+
+name = 'Ahmed Ali'
+balance = '53.44'
+text ='Dear {}, Your current  balance is {}$'
+
+print(text.format(name, balance))
